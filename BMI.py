bc57d1d43d7cfe181983acb570b159187d858977
Siddiqui <farhan.a.siddiqui@capgemini.com>
2023-10-30 12:15:46 +0530

    fifth commit

---
+++ BMI.py
@@ -0,0 +1,4 @@
+height = float(input("Enter your height:\n"))
+weight = int(input("Enter your weight:\n"))
+bmi = int(weight / (height * height))
+print("Your BMI is {bmi}")
--- Operations.py
+++ Operations.py
@@ -1,3 +1,12 @@
 # PEMDAS - (), ** , * / , + - => left to right
 
 print(3 * 3 + 3 / 3 - 3)
+
+print(round(7 / 3, 3))
+
+print(8 // 3)
+print(type(8 // 3))  # Always an integer
+
+result = 4/2
+result /=2
+print(result)
