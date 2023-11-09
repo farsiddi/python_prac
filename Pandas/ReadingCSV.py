815b38e295f9dafa78a3769584e1ef4494c5b211
Siddiqui <farhan.a.siddiqui@capgemini.com>
2023-11-09 11:30:07 +0530

    30

---
+++ Pandas/ReadingCSV.py
@@ -0,0 +1,33 @@
+# with open("weather_data.csv") as weather_data:
+#     content = weather_data.readlines()
+#     print(content)
+
+# import csv
+#
+# with open("weather_data.csv") as weather_data:
+#     weather_data = csv.reader(weather_data)
+#     temperature = []
+#     for row in weather_data:
+#         print(row)
+#         if row[1] != "temp":  # Excluding label temp so that it can be converted into integer
+#             temperature.append(int(row[1]))
+# print(temperature)
+
+# Just to take temp we have right above lines of code so that's why we use Pandas library
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
+print(type(data))  # This is a dataframe(rows and column)
+print(data["temp"])  # Takes the first row as label and this is a series(a single column)
+
+data_dict = data.to_dict()
+print(data_dict)  # Coverts into dictionary where each column is itself is a dictionary and all the columns(dictionaries) are nested inside another dictionary
+data_list = data["temp"].to_list()
+print(data_list)
+sum = 0
+for n in data_list:
+    sum += n
+
+average_temp = round(sum / len(data_list),2)
+print(average_temp)
