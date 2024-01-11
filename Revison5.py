444abb2b1e1f6fe9a7d5087bff8249f8709d0aeb
farhsidd <farhann2207@gmail.com>
2024-01-11 11:04:34 +0530

    Revision

--- Revison5.py
+++ Revison5.py
@@ -13,15 +13,33 @@ class Student:
     #     self.grades = (87, 98, 78, 79)
     def __init__(self, name, grades):
         self.name = name
-        self.grades = (grades)
+        self.grades = grades
 
     def average(self):
         return sum(self.grades) / len(self.grades)
 
 
 student = Student("Farhan", (87, 98, 78, 79))
-print(student.name)
+student1 = Student("Sanchay", (89, 67, 98, 93))
+
+
+# print(student.name)
+# print(student1.name)
 # print(student.grades)
 # print(sum(student.grades) / len(student.grades))
 # print(Student.average(student))
-print(student.average())
+# print(student.average())
+# print(student1.average())
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return "Hello"
+
+
+bob = Person("Bob", 35)
+print(bob)
